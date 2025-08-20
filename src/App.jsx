eafdbacd672d9e755343807ab00ab514a3603463
Md Amir Sohail <94072183+amirsohail66@users.noreply.github.com>
2025-08-20 06:24:37 +0000

    Google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import { BrowserRouter } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 import {
-    About,
-    Contact,
-    Experience,
-    Profile,
-    Navbar,
-    SkillsSection,
-    Works,
+  About,
+  Contact,
+  Experience,
+  Profile,
+  Navbar,
+  SkillsSection,
+  Works,
 } from "./components";
+import ReactGA from "react-ga4";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <div className="relative z-0 bg-primary">
-                <div className="bg-hero-pattern  bg-cover bg-center bg-no-repeat">
-                    <Navbar />
-                    <Profile />
-                </div>
-                <About />
-                <SkillsSection />
-                <Experience />
-                <Works />
-                <Contact />
-            </div>
-        </BrowserRouter>
-    );
+  const location = useLocation();
+
+  // ✅ Track page views
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return (
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
+        <Navbar />
+        <Profile />
+      </div>
+      <About />
+      <SkillsSection />
+      <Experience />
+      <Works />
+      <Contact />
+    </div>
+  );
 };
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-
-import App from "./App";
+import { createRoot } from "react-dom/client";
 import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ReactGA from "react-ga4";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Initialize Google Analytics
+ReactGA.initialize("G-HSDN2YLKBJ");
+
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
+
+// Performance measurement
+reportWebVitals();
